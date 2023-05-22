81e0f4ecde8062fe04bf54bf10484692521440a5
rektdie <rektdie@gmail.com>
2023-05-22 23:02:12 +0200

    Made a gui for the site

--- index.js
+++ index.js
@@ -1,54 +1,79 @@
+const playerScore = document.querySelector(".score-player");
+const computerScore = document.querySelector(".score-computer");
+let finished = false;
+
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    const computerButtons = document.querySelectorAll(".computer");
+    computerButtons.forEach(button => {
+        if (button.textContent === computerSelection) {
+            button.classList.add("clicked");
+        } else {
+            button.classList.remove("clicked");
+        }
+    });
+
+    pSelection = playerSelection.toLowerCase();
+    cSelection = computerSelection.toLowerCase();
     let result;
 
     if (playerSelection === computerSelection) {
-        return "Draw";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        result = "Draw";
+    } else if (pSelection === "rock" && cSelection === "scissors") {
         result = "You won!";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
+    } else if (pSelection === "rock" && cSelection === "paper") {
         result = "You lost!";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (pSelection === "scissors" && cSelection === "paper") {
         result = "You won!";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    } else if (pSelection === "scissors" && cSelection === "rock") {
         result = "You lost!";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (pSelection === "paper" && cSelection === "rock") {
         result = "You won!";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    } else if (pSelection === "paper" && cSelection === "scissors") {
         result = "You lost!";
     }
 
     if (result === "You won!"){
-        result += ` ${playerSelection} beats ${computerSelection}`;
-    } else {
-        result += ` ${computerSelection} beats ${playerSelection}`;
+        result += ` ${playerSelection} beats ${computerSelection}.`;
+        playerScore.textContent++;
+    } else if (result === "You lost!"){
+        result += ` ${computerSelection} beats ${playerSelection}.`;
+        computerScore.textContent++;
     }
 
-    return result;
-}
+    const resultsDiv = document.querySelector(".results");
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while (playerScore < 5 && computerScore < 5){
-        const round = playRound(prompt(), getComputerChoice());
-        if (round.includes("You won!")) {
-            playerScore++;
-        } else if (round.includes("You lost!")){
-            computerScore++;
+    if (computerScore.textContent < 5 && playerScore.textContent < 5) {
+        resultsDiv.textContent = result;
+    } else {
+        if (Number(playerScore.textContent) > Number(computerScore.textContent)){
+            resultsDiv.textContent = `Congratulations, you won the full game with a score of: ${playerScore.textContent} - ${computerScore.textContent}!`;
+        } else {
+            resultsDiv.textContent = `Unfortunately, you lost the full game with a score of: ${playerScore.textContent} - ${computerScore.textContent}!`;
         }
-        console.log(round + " " + playerScore + " - " + computerScore)
-    }
-
-    if (playerScore > computerScore){
-        return "You won the full game!";
-    } else {
-        return "The computer won the full game!";
+        finished = true;
     }
 }
+
+const buttons = document.querySelectorAll(".player");
+buttons.forEach(button => button.addEventListener("click", (e) => {
+    if (finished) {
+        playerScore.textContent = "0";
+        computerScore.textContent = "0";
+        finished = false;
+    }
+
+    for (item of buttons) {
+        item.classList.remove("clicked");
+    }
+
+    let choice = getComputerChoice();
+    choice = choice.charAt(0).toUpperCase() + choice.slice(1);
+
+    playRound(button.textContent, choice);
+    e.target.classList.add("clicked");
+}));
